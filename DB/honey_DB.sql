d8383237252c2a291ef3f21a0e044b04921253ed
Fredj21 <frederic@jelmoni.fr>
2017-03-25 06:34:21 +0100

    html modif

--- DB/honey_DB.sql
+++ DB/honey_DB.sql
@@ -1,22 +1,22 @@
-DROP DATABASE IF EXISTS honey;
-DROP USER IF EXISTS 'honey_user'@'localhost';
+#DROP DATABASE IF EXISTS honey;
+#DROP USER IF EXISTS 'honey_user'@'localhost';
 
 
-CREATE DATABASE honey;
+#CREATE DATABASE honey;
 
 
 CREATE USER 'honey_user'@'localhost' IDENTIFIED BY 'honey_pass';
 GRANT ALL ON honey.* TO 'honey_user'@'localhost';
 FLUSH PRIVILEGES;
 
-USE honey;
-DROP TABLE IF EXISTS `telnet`;
-CREATE TABLE `telnet` (
-  `date` datetime NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  `login` varchar(30) NOT NULL,
-  `pass` varchar(30) NOT NULL,
-  `timeout` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+#USE honey;
+#DROP TABLE IF EXISTS `telnet`;
+#CREATE TABLE `telnet` (
+#  `date` datetime NOT NULL,
+#  `ip` varchar(15) NOT NULL,
+#  `login` varchar(30) NOT NULL,
+#  `pass` varchar(30) NOT NULL,
+#  `timeout` tinyint(1) NOT NULL DEFAULT '0'
+#) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
