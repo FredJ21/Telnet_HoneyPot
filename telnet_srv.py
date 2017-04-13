eb952a57f5b8ca035cabd1b04bb8654763be0583
root <root@localhost.localdomain>
2017-04-13 17:48:09 +0200

    Fix Bug CPU

--- telnet_srv.py
+++ telnet_srv.py
@@ -20,49 +20,68 @@ colD = '\033[0;39m' # Default
 
 class ClientThread(threading.Thread):
 
-    def __init__(self, ip, port, clientsocket):
+    def __init__(self, ip, port, clientsocket, Nb):
 
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.clientsocket = clientsocket
         self.clientsocket.settimeout(TIMEOUT)
-        #print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
+        self.Nb = Nb
+        print("[+] New thread for %s %s %d" % (self.ip, self.port, self.Nb))
+
 
     def run(self): 
-  
+
+        # test the first sent data 
+        #self.clientsocket.settimeout(5)
+        try:
+            r = self.clientsocket.recv(2048)
+        except socket.timeout:
+            pass
+
+        #self.clientsocket.settimeout(5)
         try:
 
-            # test the first sent data 
-            self.clientsocket.settimeout(1)
-            try:
-                r = self.clientsocket.recv(2048)
-            except socket.timeout:
-                pass
-            self.clientsocket.settimeout(TIMEOUT)
+            login = ""
+            password = ""
 
-
+            # ----------------------------------------------
             # login
             self.clientsocket.send("login: ")
-            login = ""
+            a = 0
             while True:
                 rep = self.clientsocket.recv(2048)
                 login += rep
+                print("%d - 1 - %d" % (self.Nb, a))
                 if login[-1:] == '\n':
                     break
+                if a > 200 :
+                    break
+                a += 1
+                time.sleep(0.01)
+                self.clientsocket.send(str(a))
             login = login.rstrip('\n\r')
 
+
+            # ----------------------------------------------
             # password
             self.clientsocket.send("password: ")
-            password = ""
+            a = 0
             while True:
                 rep = self.clientsocket.recv(2048)
                 password += rep
                 if password[-1:] == '\n':
                     break
+                if a > 200 :
+                    break
+                a += 1
+                time.sleep(0.01)
             password = password.rstrip('\n\r')
 
-            
+
+
+            # ----------------------------------------------
             # Output
             DATE = str(time.strftime('%Y-%m-%d %H:%M:%S')) +' - '+ str(time.time())
             print ("%s - TCP %s - Connexion from ip[%s%s%s] port[%s] login[%s%s%s] pass[%s%s%s]" % (DATE, PORT, colB, self.ip, colD, self.port, colG, login, colD, colR, password, colD))
@@ -71,12 +90,18 @@ class ClientThread(threading.Thread):
             self.clientsocket.send("Login incorrect !\n")
             self.clientsocket.close()
 
-        except socket.timeout:
+        #except socket.timeout:
+        except :
             DATE = str(time.strftime('%Y-%m-%d %H:%M:%S')) +' - '+ str(time.time())
             print ("%s - TCP %s - Connexion from ip[%s%s%s] port[%s] %sTimeout !!!%s" % (DATE, PORT, colB, self.ip, colD, self.port, colR, colD))
             sys.stdout.flush()
             self.clientsocket.close()
 
+        print("[+] End thread pour %s %s %d" % (self.ip, self.port, self.Nb))
+
+
+
+# ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 DATE = str(time.strftime('%Y%m%d-%H:%M:%S')) +' - '+ str(time.time())
 
@@ -87,11 +112,19 @@ tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcpsock.bind(("",PORT))
 
+Nb = 0;
+
+
 while True:
     tcpsock.listen(10)
     (clientsocket, (ip, port)) = tcpsock.accept()
-    newthread = ClientThread(ip, port, clientsocket)
+
+#    print ("Nb = %d \n" % (Nb))
+
+    newthread = ClientThread(ip, port, clientsocket, Nb)
     newthread.start()
+
+    Nb += 1
 
 
 print ("END")
